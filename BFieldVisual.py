ee071d434a6cb15521454aebd92cac75b93d45d3
katieinder <Katie@Katies-MacBook-Air.local>
2017-01-23 18:11:23 +0000

    trying to get quiver to come on the pygame window. Believe I need to make into an image, render it and blit it to screen. But there must be another way

--- BFieldVisual.py
+++ BFieldVisual.py
@@ -1,21 +1,36 @@
-'''Quiver Practice'''
+'''Quiver in pygame'''
 
 #import libraries
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_agg as agg
+import pygame
+
+pygame.init()
+
+size=[800,600]
+screen=pygame.display.set_mode(size)
+pygame.display.set_caption('quiver experiment')
+
+clock=pygame.time.Clock()
+
+done=False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+
+    x = np.linspace(10, 20, 11)
+    y = np.linspace(10, 20, 11)
+    X, Y = np.mgrid[10:20, 10:20]
+    u = 10 * X
+    v = 10 * Y
+    q = plt.quiver(X, Y, u, v, scale=1000, color='m')
 
 
-x=np.linspace(0,10,11)
-y=np.linspace(0,10,11)
-X, Y =np.mgrid[0:10,0:10]
-#increasing number of points increases number of arrows
-u=10*X
-v=10*Y
-#increasing constant before increases size of arrows, i.e length
-#having 1*X and 10*Y make the arrows point more in the positive direction
-q=plt.quiver(X,Y,u,v, scale=1000, color='m')
-#decreasing scale reduces number of arrows and increases their length
-#p=plt.quiverkey(q, 1,16.5,50,label='ex')
-#don't see reason for quiver key yet
+    pygame.display.flip()
+    clock.tick(60)
 
-plt.show()
