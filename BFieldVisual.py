c891618fb649dcdbaa62831781c0ad70f60b3e58
katieinder <Katie@Katies-MacBook-Air.local>
2017-01-23 18:11:48 +0000

    experiments changing variables

--- BFieldVisual.py
+++ BFieldVisual.py
@@ -4,18 +4,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-x=np.linspace(0,10,11)
-y=np.linspace(0,10,11)
-X, Y =np.mgrid[0:10,0:10]
+x=np.linspace(10,20,11)
+y=np.linspace(10,20,11)
+X, Y =np.mgrid[0:11,0:11]
 #increasing number of points increases number of arrows
-u=np.cos(np.pi*X)
-v=np.sin(np.pi*Y)
+u=10*np.cos(np.pi*X)
+v=10*np.sin(np.pi*Y)
 #increasing constant before increases size of arrows, i.e length
 #having 1*X and 10*Y make the arrows point more in the positive direction
-q=plt.quiver(X,Y,u,v, scale=10, color='m')
+q=plt.quiver(X,Y,u,v, scale=100, color='m')
 #decreasing scale reduces number of arrows and increases their length
-#p=plt.quiverkey(q, 1,16.5,50,label='ex')
+#p=plt.quiverkey(q, 5,5,5,label='ex')
 #don't see reason for quiver key yet
 
 plt.show()
