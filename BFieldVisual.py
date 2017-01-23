a3a0751f1474cdc9c86fdc2c9cd533e66dff5deb
katieinder <Katie@Katies-MacBook-Air.local>
2017-01-23 17:48:32 +0000

    changing to sin and cos functions

--- BFieldVisual.py
+++ BFieldVisual.py
@@ -9,11 +9,11 @@ x=np.linspace(0,10,11)
 y=np.linspace(0,10,11)
 X, Y =np.mgrid[0:10,0:10]
 #increasing number of points increases number of arrows
-u=10*X
-v=10*Y
+u=np.cos(np.pi*X)
+v=np.sin(np.pi*Y)
 #increasing constant before increases size of arrows, i.e length
 #having 1*X and 10*Y make the arrows point more in the positive direction
-q=plt.quiver(X,Y,u,v, scale=1000, color='m')
+q=plt.quiver(X,Y,u,v, scale=10, color='m')
 #decreasing scale reduces number of arrows and increases their length
 #p=plt.quiverkey(q, 1,16.5,50,label='ex')
 #don't see reason for quiver key yet
