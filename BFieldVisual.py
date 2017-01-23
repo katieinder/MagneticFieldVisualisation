0887ee7a2aafc0f2caeaaebc2ce29a12b5d34537
katieinder <Katie@Katies-MacBook-Air.local>
2017-01-23 17:44:02 +0000

    discovering the role of each parameter

--- BFieldVisual.py
+++ BFieldVisual.py
@@ -4,11 +4,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 x=np.linspace(0,10,11)
 y=np.linspace(0,10,11)
-X, Y =np.mgrid[0:11,0:11]
-u=5*X
-v=5*Y
-q=plt.quiver(X,Y,u,v,angles='xy', scale=1000, color='r')
-p=plt.quiverkey(q, 1,16.5,50, label="50m/s", coordinates='data', color='r')
+X, Y =np.mgrid[0:10,0:10]
+#increasing number of points increases number of arrows
+u=10*X
+v=10*Y
+#increasing constant before increases size of arrows, i.e length
+#having 1*X and 10*Y make the arrows point more in the positive direction
+q=plt.quiver(X,Y,u,v, scale=1000, color='m')
+#decreasing scale reduces number of arrows and increases their length
+#p=plt.quiverkey(q, 1,16.5,50,label='ex')
+#don't see reason for quiver key yet
+
 plt.show()
